8cc81eca8a9622ae7cbf98e9486b01be372d4f48
Tushar-Wagdare <rahastugawa@gmail.com>
2025-01-31 18:21:08 +0530

    Added -> Instancing is done ...

---
+++ OGL.cpp
@@ -0,0 +1,364 @@
+#include <windows.h>
+#include <windowsx.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <iostream>
+
+#include <GL/glew.h>
+#include <gl/GL.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+#include "WindowManager.h"
+#include "Logger.h"
+#include "Timer.h"
+#include "camera.h"
+#include "Shader.h"
+#include "model.h"
+
+
+
+//*** Globle Function Declarations ***
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+
+//*** Global Variable Declaration ***
+WindowManager* pWindow = NULL;
+Camera* camera = NULL;
+float lastX = WindowManager::SCR_WIDTH / 2.0f;
+float lastY = WindowManager::SCR_HEIGHT / 2.0f;
+bool firstMouse = true;
+// timing
+float deltaTime = 0.0f;	// time between current frame and last frame
+float lastFrame = 0.0f;
+
+
+
+///==================== OpenGL Variables =======================///
+GLuint vaoCube = 0;
+GLuint vbo_position_cube = 0;
+GLuint vbo_normal_cube = 0;
+
+GLfloat angleCube = 0.0f;
+// lighting
+glm::vec3 lightPos(1.2f, 1.0f, 2.0f);
+
+
+
+
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int iCmdShow)
+{
+	unsigned int loadTexture(char const* path);
+	pWindow = new WindowManager();
+	MSG msg = { 0 };
+	camera = new Camera();
+
+	Logger::Init();
+
+	TIMER_INIT("Window");
+	pWindow->initialize();
+	TIMER_END(); 
+	LOG_INFO("Window Initialized in %.6f seconds", TIMER_GET("Window"));
+
+
+
+	///======================== OpenGL INIT ==============================///
+	glEnable(GL_DEPTH_TEST);
+
+	// build and compile shaders
+	// -------------------------
+	Shader asteroidShader("shaders/asteroids.vs", "shaders/asteroids.fs");
+	Shader planetShader("shaders/planet.vs", "shaders/planet.fs");
+
+	// load models
+	// -----------
+	Model rock("resources/objects/rock/rock.obj");
+	Model planet("resources/objects/planet/planet.obj");
+
+	// generate a large list of semi-random model transformation matrices
+	// ------------------------------------------------------------------
+	unsigned int amount = 100000;
+	glm::mat4* modelMatrices;
+	modelMatrices = new glm::mat4[amount];
+	srand(static_cast<unsigned int>(Timer::getAppRunTime())); // initialize random seed
+	float radius = 150.0;
+	float offset = 25.0f;
+	for (unsigned int i = 0; i < amount; i++)
+	{
+		glm::mat4 model = glm::mat4(1.0f);
+		// 1. translation: displace along circle with 'radius' in range [-offset, offset]
+		float angle = (float)i / (float)amount * 360.0f;
+		float displacement = (rand() % (int)(2 * offset * 100)) / 100.0f - offset;
+		float x = sin(angle) * radius + displacement;
+		displacement = (rand() % (int)(2 * offset * 100)) / 100.0f - offset;
+		float y = displacement * 0.4f; // keep height of asteroid field smaller compared to width of x and z
+		displacement = (rand() % (int)(2 * offset * 100)) / 100.0f - offset;
+		float z = cos(angle) * radius + displacement;
+		model = glm::translate(model, glm::vec3(x, y, z));
+
+		// 2. scale: Scale between 0.05 and 0.25f
+		float scale = static_cast<float>((rand() % 20) / 100.0 + 0.05);
+		model = glm::scale(model, glm::vec3(scale));
+
+		// 3. rotation: add random rotation around a (semi)randomly picked rotation axis vector
+		float rotAngle = static_cast<float>((rand() % 360));
+		model = glm::rotate(model, rotAngle, glm::vec3(0.4f, 0.6f, 0.8f));
+
+		// 4. now add to list of matrices
+		modelMatrices[i] = model;
+	}
+
+	// configure instanced array
+	// -------------------------
+	unsigned int buffer;
+	glGenBuffers(1, &buffer);
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	glBufferData(GL_ARRAY_BUFFER, amount * sizeof(glm::mat4), &modelMatrices[0], GL_STATIC_DRAW);
+
+	// set transformation matrices as an instance vertex attribute (with divisor 1)
+	// note: we're cheating a little by taking the, now publicly declared, VAO of the model's mesh(es) and adding new vertexAttribPointers
+	// normally you'd want to do this in a more organized fashion, but for learning purposes this will do.
+	// -----------------------------------------------------------------------------------------------------------------------------------
+	for (unsigned int i = 0; i < rock.meshes.size(); i++)
+	{
+		unsigned int VAO = rock.meshes[i].VAO;
+		glBindVertexArray(VAO);
+		// set attribute pointers for matrix (4 times vec4)
+		glEnableVertexAttribArray(3);
+		glVertexAttribPointer(3, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (void*)0);
+		glEnableVertexAttribArray(4);
+		glVertexAttribPointer(4, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (void*)(sizeof(glm::vec4)));
+		glEnableVertexAttribArray(5);
+		glVertexAttribPointer(5, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (void*)(2 * sizeof(glm::vec4)));
+		glEnableVertexAttribArray(6);
+		glVertexAttribPointer(6, 4, GL_FLOAT, GL_FALSE, sizeof(glm::mat4), (void*)(3 * sizeof(glm::vec4)));
+
+		glVertexAttribDivisor(3, 1);
+		glVertexAttribDivisor(4, 1);
+		glVertexAttribDivisor(5, 1);
+		glVertexAttribDivisor(6, 1);
+
+		glBindVertexArray(0);
+	}
+
+
+
+
+
+	//*** Game LOOP ***
+	while (pWindow->isRunning == FALSE)
+	{
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
+		{
+			if (msg.message == WM_QUIT)
+				pWindow->isRunning = TRUE;
+			else
+			{
+				TranslateMessage(&msg);
+				DispatchMessage(&msg);
+			}
+		}
+		else
+		{
+			///=========================== DISPLAY ==================================//
+			// per-frame time logic
+			// --------------------
+			float currentFrame = static_cast<float>(Timer::getAppRunTime() * 0.05f);
+			deltaTime = currentFrame - lastFrame;
+			lastFrame = currentFrame;
+
+
+			glClearColor(0.1f, 0.1f, 0.1f, 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+			// configure transformation matrices
+			glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)WindowManager::SCR_WIDTH / (float)WindowManager::SCR_HEIGHT, 0.1f, 1000.0f);
+			glm::mat4 view = camera->GetViewMatrix();
+			asteroidShader.use();
+			asteroidShader.setMat4("projection", projection);
+			asteroidShader.setMat4("view", view);
+			planetShader.use();
+			planetShader.setMat4("projection", projection);
+			planetShader.setMat4("view", view);
+
+			// draw planet
+			glm::mat4 model = glm::mat4(1.0f);
+			model = glm::translate(model, glm::vec3(0.0f, -3.0f, 0.0f));
+			model = glm::scale(model, glm::vec3(4.0f, 4.0f, 4.0f));
+			planetShader.setMat4("model", model);
+			planet.Draw(planetShader);
+
+			// draw meteorites
+			asteroidShader.use();
+			asteroidShader.setInt("texture_diffuse1", 0);
+			glActiveTexture(GL_TEXTURE0);
+			glBindTexture(GL_TEXTURE_2D, rock.textures_loaded[0].id); // note: we also made the textures_loaded vector public (instead of private) from the model class.
+			for (unsigned int i = 0; i < rock.meshes.size(); i++)
+			{
+				glBindVertexArray(rock.meshes[i].VAO);
+				glDrawElementsInstanced(GL_TRIANGLES, static_cast<unsigned int>(rock.meshes[i].indices.size()), GL_UNSIGNED_INT, 0, amount);
+				glBindVertexArray(0);
+			}
+
+
+			pWindow->swapDisplayBuffer();
+
+			///================== UPDATE =======================//
+			//angleCube = angleCube + 0.02f;
+
+		}
+	}
+
+
+	return((int)msg.wParam);
+}
+
+
+LRESULT CALLBACK WndProc(HWND hwnd, UINT iMsg, WPARAM wParam, LPARAM lParam)
+{
+	//*** Function Declaration ***
+	void ToggleFullscreen(void);
+	void resize(int, int);
+
+
+	//*** Code ***
+	switch (iMsg)
+	{
+	case WM_SETFOCUS:
+		break;
+
+	case WM_KILLFOCUS:
+		break;
+
+	case WM_SIZE:
+        glViewport(0, 0, LOWORD(lParam), HIWORD(lParam));
+		break;
+
+	case WM_ERASEBKGND:
+		return(0);
+
+	case WM_MOUSEMOVE:
+		{
+			float xpos = static_cast<float>(GET_X_LPARAM(lParam));
+			float ypos = static_cast<float>(GET_Y_LPARAM(lParam));
+
+			if (firstMouse)
+			{
+				lastX = xpos;
+				lastY = ypos;
+				firstMouse = false;
+			}
+
+			float xoffset = xpos - lastX;
+			float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+
+			lastX = xpos;
+			lastY = ypos;
+			camera->ProcessMouseMovement(xoffset, yoffset);
+		}
+		
+		break;
+
+	case WM_KEYDOWN:
+		switch (LOWORD(wParam))
+		{
+		case VK_ESCAPE:
+			DestroyWindow(hwnd);
+			break;
+		}
+		break;
+
+	case WM_CHAR:
+		switch (LOWORD(wParam))
+		{
+		case 'F':
+		case 'f':
+			
+			break;
+
+		case 'W':
+		case 'w':
+			camera->ProcessKeyboard(FORWARD, deltaTime);
+			break;
+
+		case 'A':
+		case 'a':
+			camera->ProcessKeyboard(LEFT, deltaTime);
+			break;
+
+		case 'S':
+		case 's':
+			camera->ProcessKeyboard(BACKWARD, deltaTime);
+			break;
+
+		case 'D':
+		case 'd':
+			camera->ProcessKeyboard(RIGHT, deltaTime);
+			break;
+
+		
+		}
+		break;
+
+	case WM_CLOSE:
+		DestroyWindow(hwnd);
+		break;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+
+	default:
+		break;
+	}
+
+
+	return(DefWindowProc(hwnd, iMsg, wParam, lParam));
+}
+
+
+unsigned int loadTexture(char const* path)
+{
+	unsigned int textureID;
+	glGenTextures(1, &textureID);
+
+	int width, height, nrComponents;
+	unsigned char* data = stbi_load(path, &width, &height, &nrComponents, 0);
+	if (data)
+	{
+		GLenum format;
+		if (nrComponents == 1)
+			format = GL_RED;
+		else if (nrComponents == 3)
+			format = GL_RGB;
+		else if (nrComponents == 4)
+			format = GL_RGBA;
+
+		glBindTexture(GL_TEXTURE_2D, textureID);
+		glTexImage2D(GL_TEXTURE_2D, 0, format, width, height, 0, format, GL_UNSIGNED_BYTE, data);
+		glGenerateMipmap(GL_TEXTURE_2D);
+
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+		stbi_image_free(data);
+	}
+	else
+	{
+		std::cout << "Texture failed to load at path: " << path << std::endl;
+		stbi_image_free(data);
+	}
+
+	return textureID;
+}
+
